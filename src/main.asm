7c66f109e618be12b9ae0547143201ad4268b57b
exobition <116936838+exobition@users.noreply.github.com>
2026-01-05 17:20:29 -0800

    adding comments to explain what is going on

--- src/main.asm
+++ src/main.asm
@@ -1,11 +1,11 @@
-org 0x7c00
-bits 16
+org 0x7c00 ; Offsetting to 31744 hexadecimal
+bits 16 ; setting bits to 16 (for the compiler)
 
 main:
-  hlt
+  hlt ; halting cpu
 
 .halt:
-  jmp .halt
+  jmp .halt ; looping in case of an interrupt
 
-times 510-($-$$) db 0 
-dw 0xAA55
+times 510-($-$$) db 0 ; fill the empty space with 0
+dw 0xAA55 ; what the bios looks for to see if a disk is bootable
